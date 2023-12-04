0198e1a86e674065e27b6bcf506e7b92983efdbe
W0bW0b <141922660+W0bW0b@users.noreply.github.com>
2023-12-04 13:33:32 +0100

    api cal funktioniert

--- src/app/component/search-page/search-page.component.ts
+++ src/app/component/search-page/search-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MarsimagesService } from '../service/marsimages.service';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.css']
 })
-export class SearchPageComponent {
+export class SearchPageComponent implements OnInit {
+  marsImages: any[] | undefined; // Typisiere dies entsprechend den erwarteten Daten
+
+  constructor(private marsService: MarsimagesService) {}
+
+  ngOnInit() {
+    this.marsService.getImages().subscribe(
+      (images) => {
+        this.marsImages = images;
+        console.log(images)
+      },
+      (error) => {
+        console.error('Error fetching Mars images:', error);
+      }
+    );
+  }
+
+  crateImageBox(img){
+
+  }
+
 
 }
