64b70b1b78e00b54a9d398ea84505ef5caf6f605
W0bW0b <141922660+W0bW0b@users.noreply.github.com>
2023-12-12 16:00:31 +0100

    created sort function.

--- src/app/component/notAPage/image-box/image-box.component.ts
+++ src/app/component/notAPage/image-box/image-box.component.ts
@@ -9,8 +9,10 @@ import {MarsimagesService} from "../../service/marsimages.service";
 })
 export class ImageBoxComponent implements OnInit {
   marsImageResult: marsImage[] = [];
+  sortStyle: "latestDate" | "oldestDate" | "highestResolution" | "lowestResolution" = "latestDate";
 
-  constructor(private marsService: MarsimagesService) {}
+  constructor(private marsService: MarsimagesService) {
+  }
 
   ngOnInit() {
     this.reload();
@@ -43,10 +45,38 @@ export class ImageBoxComponent implements OnInit {
         console.error('Error fetching Mars images:', error);
       }
     );
-  }
-  sortedOldest(imageList: any[]){
 
+  this.sortMarsImageResult(this.sortStyle);
   }
+
+  sortMarsImageResult(sortStyle: "latestDate" | "oldestDate" | "highestResolution" | "lowestResolution") {
+    this.sortStyle = sortStyle;
+    switch (this.sortStyle) {
+      case "latestDate":
+        this.marsImageResult = this.marsImageResult.slice().sort((a , b) => Number(a.id) - Number(b.id));
+        break;
+
+      case "oldestDate":
+        this.marsImageResult = this.marsImageResult.slice().sort((a , b) => Number(b.id) - Number(a.id));
+        break;
+
+      case "highestResolution":
+
+        this.marsImageResult = this.marsImageResult.slice().sort((a , b) => this.calculateImageResolution(a.img_src) - this.calculateImageResolution(b.img_src));
+        break;
+
+      case "lowestResolution":
+        this.marsImageResult = this.marsImageResult.slice().sort((a , b) => this.calculateImageResolution(b.img_src) - this.calculateImageResolution(a.img_src));
+        break;
+    }
+  }
+
+  calculateImageResolution(imageLink: string): number {
+    const img = new Image();
+    img.src = imageLink;
+    return img.width * img.height;
+  }
+
 }
 
 
